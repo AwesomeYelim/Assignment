0bdb9c0481e9837e08060ed2bee9ee7b8832c760
Awesomeyelim <uiop01900@gmail.com>
2024-02-02 13:52:33 +0900

    주석추가 & detail

--- main/client.go
+++ main/client.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 	"time"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8080") // 1
+	conn, err := net.Dial("tcp", "localhost:8080") // Dial : 전화걸다
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -15,35 +16,36 @@ func main() {
 
 	defer conn.Close()
 	go func(c net.Conn) {
-		send := []string{"피카츄", "라이츄", "파이리", "꼬부기"}
+		send := []string{"피카츄", "라이츄", "파이리", "꼬부기", "버터풀", "야도란"}
 		for i := range send {
 			_, err = c.Write([]byte(send[i]))
+
 			if err != nil {
 				fmt.Println("Failed to write data : ", err)
 				break
 			}
-			if i == len(send)-1 {
-				defer conn.Close()
-				return
-			}
+			defer conn.Close()
+
 			i++
-			time.Sleep(5 * time.Second)
+			time.Sleep(3 * time.Second)
 
 		}
 	}(conn)
 
+	// 내부 동기식 처리로 전송(쓰기)되고 나서 연결이 끊어지는 현상이 생겨 따로 함수로 비동기식 처리
 	go func(c net.Conn) {
 		recv := make([]byte, 4096)
 
 		for {
 			n, err := c.Read(recv)
 			if err != nil {
-				fmt.Println(err)
+				os.Exit(0) // 인터럽트 신호로 터미널 닫음
+				//fmt.Println(err)
 				return
 			}
 			fmt.Println("client : ", string(recv[:n]))
 		}
 	}(conn)
 
-	fmt.Scanln()
+	fmt.Scanln() // 고루틴을 이어나가기 위해 fmt.Scanln() 넣어줌
 }
--- main/server.go
+++ main/server.go
@@ -3,30 +3,25 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 )
 
 func handler(conn net.Conn) {
-	buffer := make([]byte, 1024)
+	buffer := make([]byte, 1024) // 데이터를 담을 buffer 생성
 
 	for {
-		n, err := conn.Read(buffer)
+		n, err := conn.Read(buffer) // conn은 메모리 주소값을 참조한다. 루프를 도는 이유는 순차적으로  읽기 => 쓰기(보내기) 를 진행하기 위함
 		if err != nil {
-			fmt.Println(err)
+			defer os.Exit(0) // 인터럽트 신호로 터미널 닫음
+			//fmt.Println(err)
 			return
 		}
 		if n > 0 {
 			fmt.Println("server : ", string(buffer[:n]))
-			conn.Write(buffer[:n])
+			conn.Write(buffer[:n]) // buffer 형식 내 데이터 값이 있는 index 까지 잘라주어 client 로 보낸다. => byte 형식으로 보내야함
 		}
-	}
 
-	//fmt.Println(string(buffer[:n]))
-	//
-	//if err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-	//defer conn.Close()
+	}
 
 }
 
@@ -37,7 +32,7 @@ func main() {
 		return
 	}
 	defer listener.Close()
-
+	
 	for { // 무한 루프를 생성한다. 들어오는 연결을 지속적으로 수신해야하는 서버의 일반적인 패턴임
 		conn, err := listener.Accept() // 접속을 대기한다. => 사용자 접속시 handler 함수로 해당 커넥션을 처리한다.
 		if err != nil {
@@ -46,5 +41,4 @@ func main() {
 		}
 		go handler(conn)
 	}
-
 }
